405f658db4c776a2e0c093f83207aace51f83368
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2015-05-17 00:02:15 +0200

    add test to obuild

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -24,5 +24,22 @@ namespace OpenContentTests
             Assert.AreEqual(expected, res);
 
         }
+
+        [TestMethod]
+        public void DivideHelper()
+        {
+
+            string expected = "2";
+            //string dataJson = "{\"lst\":[{\"data\":1},{\"data\":2},{\"data\":3}]}";
+
+            //dynamic model = JsonUtils.JsonToDynamic(dataJson);
+            string source = "{{divide data \"5\"}}";
+            dynamic model = new { data = 10};
+            HandlebarsEngine hbEngine = new HandlebarsEngine();
+            string res = hbEngine.Execute(source, model);
+
+            Assert.AreEqual(expected, res);
+
+        }
     }
 }
