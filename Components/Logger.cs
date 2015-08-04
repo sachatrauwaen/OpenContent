c23e4151da2c0ff3bd4ec171b274dc30e2ae2d2c
Robrecht Siera <robrecht.siera@holoncom.eu>
2015-08-04 12:34:21 +0200

    Logger functionality (basic)

---
+++ Components/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using ClientDependency.Core;
+using DotNetNuke.Instrumentation;
+
+namespace Satrabel.OpenContent.Components
+{
+    /// <summary>
+    /// Utility class containing several commonly used procedures by Stefan Kamphuis
+    /// </summary>
+    public static class Logging
+    {
+        public static ILog Logger
+        {
+            get
+            {
+                return LoggerSource.Instance.GetLogger("OpenContent");
+            }
+        }
+
+        public static void LogServiceResult(HttpResponseMessage response, string responsemessage = "")
+        {
+            if (Logger.IsDebugEnabled)
+            {
+                StackTrace st = new StackTrace();
+
+                string method = st.GetFrame(1).GetMethod().Name == "CreateResponse"
+                    ? st.GetFrame(2).GetMethod().Name
+                    : st.GetFrame(1).GetMethod().Name;
+
+                Logger.DebugFormat("Result from '{0}' with status '{1}': {2} \r\n", method, response.StatusCode.ToString(), String.IsNullOrEmpty(responsemessage) ? "<empty>" : responsemessage);
+            }
+        }
+
+
+        
+    }
+}
