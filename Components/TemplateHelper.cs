aeb5d5b83c07c3d9aafa56e1b02b513649ef65f0
Robrecht Siera <robrecht.siera@holoncom.eu>
2015-08-13 17:40:37 +0200

    Helper methods to normalize dynamic variables (which could be non-existant)

--- Components/TemplateHelper.cs
+++ Components/TemplateHelper.cs
@@ -29,5 +29,52 @@ namespace Satrabel.OpenContent.Components
             ClientResourceManager.RegisterScript((Page)HttpContext.Current.CurrentHandler, filePath, FileOrder.Js.DefaultPriority);
 
         }
+
+
+        #region NormalizeDynamic
+        /// <summary>
+        /// Normalizes a setting from a Alpaca form field
+        /// </summary>
+        /// <para>
+        /// An Alpaca field that has been defined as Number and is not filled in (has no value)
+        /// will return a dynamic null value that is seen as a int with value null (not a int? but a real int with value null). 
+        /// C# manual and Resharper says an int can never be Null. But alpaca forms manages to give us a int that is null
+        /// That is very strange and akward and needs to be normalized.
+        /// </para>
+        /// <param name="value">The value.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns></returns>
+        public static int NormalizeDynamic(dynamic value, int defaultValue)
+        {
+            if (value == null) return defaultValue;
+            if (value.GetType() == 0.GetType()) return value ?? defaultValue; //Resharper says value is never Null. 
+
+            int retVal = 0;
+            if (!Int32.TryParse(value, out retVal))
+            {
+                retVal = defaultValue;
+            }
+            return retVal;
+        }
+        public static bool NormalizeDynamic(dynamic value, bool defaultValue)
+        {
+            if (value == null) return defaultValue;
+            if (value.GetType() == true.GetType()) return value ?? defaultValue; //Resharper says value is never Null. 
+
+            bool retVal;
+            if (!bool.TryParse(value, out retVal))
+            {
+                retVal = defaultValue;
+            }
+            return retVal;
+        }
+        public static string NormalizeDynamic(dynamic value, string defaultValue)
+        {
+            if (value == null) return defaultValue;
+            if (value.GetType() == "".GetType()) return value ?? defaultValue; //Resharper says value is never Null. 
+            return value.ToString();
+        }
+        #endregion
+
     }
 }
