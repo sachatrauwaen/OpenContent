1bd7914efe142243a5fca35ed6a9dfb056c6fdbd
Robrecht Siera <robrecht.siera@holoncom.eu>
2015-11-30 13:39:56 +0100

    New Normalizer for Dynamic date Value

---
+++ Components/TemplateHelpers/OtherExtentions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Satrabel.OpenContent.Components.TemplateHelpers
+{
+    public static class OtherExtentions
+    {
+        public static string ToStringOrDefault(this DateTime? source, string format = "yyyy-MM-dd hh:mm:ss", string defaultValue = null)
+        {
+            if (source != null)
+            {
+                return source.Value.ToString(format);
+            }
+            return string.IsNullOrEmpty(defaultValue) ? string.Empty : defaultValue;
+        }
+    }
+}
