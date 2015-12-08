36847b430f5f5ae10503fc5f6700a5752cbde1a9
Robrecht Siera <robrecht.siera@holoncom.eu>
2015-12-08 12:51:08 +0100

    change version nr

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("01.00.02.0")]
-[assembly: AssemblyFileVersion("01.00.02.0")]
+[assembly: AssemblyVersion("02.00.01.0")]
+[assembly: AssemblyFileVersion("02.00.01.0")]
