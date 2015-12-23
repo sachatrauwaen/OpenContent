1810f6ca049bd403b7a330116865e90f94049f56
Robrecht Siera <robrecht.siera@holoncom.eu>
2015-12-23 15:36:42 +0100

    start on IDatasource implementation

---
+++ Components/Infrastructure/DefaultDatasource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Satrabel.OpenContent.Components.Interfaces;
+using Satrabel.OpenContent.Components.Lucene;
+using Satrabel.OpenContent.Components.Manifest;
+
+namespace Satrabel.OpenContent.Components.Infrastructure
+{
+    public class DefaultDatasource : IDatasource
+    {
+
+    }
+}
