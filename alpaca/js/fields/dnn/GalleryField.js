19e57976557184b6b09612b81c5cd77ddbca5d36
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2016-03-21 10:12:13 +0100

    imagecrop field multiupload field gallery field documents field title for edit content

---
+++ alpaca/js/fields/dnn/GalleryField.js
@@ -0,0 +1,67 @@
+﻿(function ($) {
+
+    var Alpaca = $.alpaca;
+
+    Alpaca.Fields.DocumentsField = Alpaca.Fields.MultiUploadField.extend(
+    {
+        setup: function () {
+            this.base();
+            this.schema.items = {
+                "type": "object",
+                "properties": {
+                    "File": {
+                        "title": "File",
+                        "type": "string"
+                    },
+                }
+            };
+            Alpaca.merge(this.options.items, {
+                "fields": {
+                    "File": {
+                        "type": "file"
+                    },
+                }
+            });
+            this.urlfield = "File";
+        },
+        getTitle: function () {
+            return "Gallery";
+        },
+        getDescription: function () {
+            return "Image Gallery";
+        },
+
+        getSchemaOfOptions: function () {
+            return Alpaca.merge(this.base(), {
+                "properties": {
+                    "validateAddress": {
+                        "title": "Address Validation",
+                        "description": "Enable address validation if true",
+                        "type": "boolean",
+                        "default": true
+                    },
+                    "showMapOnLoad": {
+                        "title": "Whether to show the map when first loaded",
+                        "type": "boolean"
+                    }
+                }
+            });
+        },
+
+        getOptionsForOptions: function () {
+            return Alpaca.merge(this.base(), {
+                "fields": {
+                    "validateAddress": {
+                        "helper": "Address validation if checked",
+                        "rightLabel": "Enable Google Map for address validation?",
+                        "type": "checkbox"
+                    }
+                }
+            });
+        }
+
+    });
+
+    Alpaca.registerFieldClass("documents", Alpaca.Fields.DocumentsField);
+
+})(jQuery);
