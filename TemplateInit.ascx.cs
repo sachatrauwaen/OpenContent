fb14a5a0d4e33a9a72263064d22db03e16de02f8
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2016-04-15 15:50:46 +0200

    fix : exclude the modules of other languages

--- TemplateInit.ascx.cs
+++ TemplateInit.ascx.cs
@@ -288,6 +288,7 @@ namespace Satrabel.OpenContent
 
             //todo: next line should exclude the modules of other languages
             modules = modules.Where(m => m.ModuleDefinition.DefinitionName == "OpenContent" && m.IsDeleted == false && !m.OpenContentSettings().IsOtherModule);
+            
 
             rblDataSource.Items[1].Enabled = modules.Any();
             phDataSource.Visible = rblDataSource.SelectedIndex == 1; // other module
@@ -306,8 +307,24 @@ namespace Satrabel.OpenContent
                 {
                     var tc = new TabController();
                     var tab = tc.GetTab(item.TabID, ModuleContext.PortalId, false);
+                    if (!tab.IsNeutralCulture && tab.CultureCode != DnnUtils.GetCurrentCultureCode())
+                    {
+                        // skip other cultures
+                        continue;
+                    }
+
+
                     var tabpath = tab.TabPath.Replace("//", "/").TrimEnd(tab.TabName).Trim('/');
-                    var li = new ListItem(string.Format("[{2}]{0} - {1}", tab.TabName, item.ModuleTitle, tabpath), item.TabModuleID.ToString());
+                    ListItem li;
+                    if (string.IsNullOrEmpty(tabpath))
+                    {
+                        li = new ListItem(string.Format("{0} - {1}", tab.TabName, item.ModuleTitle), item.TabModuleID.ToString());
+                    }
+                    else
+                    {
+                        li = new ListItem(string.Format("[{2}]{0} - {1}", tab.TabName, item.ModuleTitle, tabpath), item.TabModuleID.ToString());
+                    }
+                    
                     listItems.Add(li);
                     if (item.TabID == tabId && item.ModuleID == moduleId)
                     {
