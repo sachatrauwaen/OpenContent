cca2725a7a0f4ab01b5f81a64669a435e7b642b1
Robrecht Siera <robrecht.siera@holoncom.eu>
2016-05-17 13:55:47 +0200

    add a NavigateUrl helper

---
+++ Components/TemplateHelpers/UrlHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetNuke.Common.Internal;
+using DotNetNuke.Entities.Portals;
+
+namespace Satrabel.OpenContent.Components.TemplateHelpers
+{
+    public static class UrlHelpers
+    {
+        public static string NavigateUrl(int targetTabId, string detailItemId, string detailItemTitle, params string[] additionalParameters)
+        {
+            if (string.IsNullOrEmpty(detailItemTitle)) return null;
+            if (targetTabId == 0) return null;
+
+            detailItemTitle = detailItemTitle.CleanupUrl();
+            string[] param = { "id", detailItemId };
+            param = param.Concat(additionalParameters).ToArray();
+            var newUrl = TestableGlobals.Instance.NavigateURL(targetTabId, false, PortalSettings.Current, string.Empty, string.Empty, detailItemTitle, param);
+            return newUrl.Length <= 230 ? newUrl : newUrl.Substring(0, 230); //actual url has ?default.aspx 
+        }
+    }
+}
