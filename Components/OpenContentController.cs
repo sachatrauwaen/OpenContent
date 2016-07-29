576544fad63aa71d79d9554b92d95a5a5d3409c3
Jan Jonas <mail@janjonas.net>
2016-07-29 21:47:20 +0200

    Add global settings to define number of max versions to keep

--- Components/OpenContentController.cs
+++ Components/OpenContentController.cs
@@ -82,7 +82,7 @@ namespace Satrabel.OpenContent.Components
             if (versions.Count == 0 || versions[0].Json.ToString() != content.Json)
             {
                 versions.Insert(0, ver);
-                if (versions.Count > 5)
+                if (versions.Count > OpenContentControllerFactory.Instance.OpenContentGlobalSettingsController.GetMaxVersions())
                 {
                     versions.RemoveAt(versions.Count - 1);
                 }
---
+++ Components/OpenContentControllerFactory.cs
@@ -0,0 +1,16 @@
+﻿using DotNetNuke.Entities.Portals;
+
+namespace Satrabel.OpenContent.Components
+{
+    public class OpenContentControllerFactory
+    {
+        private OpenContentGlobalSettingsController _openContentGlobalSettingsController;
+        public OpenContentGlobalSettingsController OpenContentGlobalSettingsController => _openContentGlobalSettingsController ??
+            (_openContentGlobalSettingsController = new OpenContentGlobalSettingsController(PortalSettings.Current.PortalId));
+
+        private OpenContentControllerFactory() { }
+
+        private static OpenContentControllerFactory _instance;
+        public static OpenContentControllerFactory Instance => _instance ?? (_instance = new OpenContentControllerFactory());
+    }
+}
