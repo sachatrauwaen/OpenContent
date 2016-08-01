77f4120a4ca4dc0cf09b34c67cd31449eed9cf64
Robrecht Siera <robrecht.siera@holoncom.eu>
2016-08-01 10:58:17 +0200

    Add helper to easily get Url of a file

---
+++ Components/Uri/UriFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Satrabel.OpenContent.Components.TemplateHelpers;
+
+namespace Satrabel.OpenContent.Components
+{
+    public static class UriFactory
+    {
+        public static PortalFileUri CreatePortalFileUri(dynamic portalFileId)
+        {
+            PortalFileUri retval = null;
+            try
+            {
+                retval = CreatePortalFileUri(Convert.ToInt32(portalFileId));
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.ErrorFormat("Error while trying to create PortalFileUri: ", ex);
+            }
+            return retval;
+        }
+
+        public static PortalFileUri CreatePortalFileUri(string portalFileId)
+        {
+            return CreatePortalFileUri(Convert.ToInt32(portalFileId));
+        }
+
+        public static PortalFileUri CreatePortalFileUri(int portalFileId)
+        {
+            PortalFileUri retval = null;
+            try
+            {
+                retval = portalFileId == 0 ? null : new PortalFileUri(portalFileId);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.ErrorFormat("Error while trying to create PortalFileUri: ", ex);
+            }
+            return retval;
+        }
+    }
+}
