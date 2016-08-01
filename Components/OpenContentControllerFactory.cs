06aa5dd48331f66d20d81d4bebf4aba89bde02df
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2016-08-01 10:09:54 +0200

    refactor for VS 2013 compatible

--- Components/OpenContentControllerFactory.cs
+++ Components/OpenContentControllerFactory.cs
@@ -5,12 +5,24 @@ namespace Satrabel.OpenContent.Components
     public class OpenContentControllerFactory
     {
         private OpenContentGlobalSettingsController _openContentGlobalSettingsController;
-        public OpenContentGlobalSettingsController OpenContentGlobalSettingsController => _openContentGlobalSettingsController ??
-            (_openContentGlobalSettingsController = new OpenContentGlobalSettingsController(PortalSettings.Current.PortalId));
+        public OpenContentGlobalSettingsController OpenContentGlobalSettingsController
+        {
+            get
+            {
+                return _openContentGlobalSettingsController ??
+                (_openContentGlobalSettingsController = new OpenContentGlobalSettingsController(PortalSettings.Current.PortalId));
+            }
+        }
 
         private OpenContentControllerFactory() { }
 
         private static OpenContentControllerFactory _instance;
-        public static OpenContentControllerFactory Instance => _instance ?? (_instance = new OpenContentControllerFactory());
+        public static OpenContentControllerFactory Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new OpenContentControllerFactory());
+            }
+        }
     }
 }
