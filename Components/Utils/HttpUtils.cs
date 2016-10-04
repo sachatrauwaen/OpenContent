3a0a325f0ca57c0e98847a29153fdd4567e99442
Robrecht Siera <robrecht.siera@holoncom.eu>
2016-10-04 12:33:39 +0200

    Start implemantation of Excel download

---
+++ Components/Utils/HttpUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Satrabel.OpenContent.Components
+{
+    public static class HttpUtils
+    {
+        public static string CreateContentDisposition(string fileName, HttpRequest request)
+        {
+            string contentDisposition;
+            if (request.Browser.Browser == "IE" && (request.Browser.Version == "7.0" || request.Browser.Version == "8.0"))
+                contentDisposition = "attachment; filename=" + Uri.EscapeDataString(fileName);
+            else if (request.UserAgent != null && request.UserAgent.ToLowerInvariant().Contains("android")) // android built-in download manager (all browsers on android)
+                contentDisposition = "attachment; filename=\"" + MakeAndroidSafeFileName(fileName) + "\"";
+            else
+                contentDisposition = "attachment; filename=\"" + fileName + "\"; filename*=UTF-8''" + Uri.EscapeDataString(fileName);
+            return contentDisposition;
+        }
+
+        private static readonly Dictionary<char, char> AndroidAllowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._-+,@£$€!½§~'=()[]{}0123456789".ToDictionary(c => c);
+        private static string MakeAndroidSafeFileName(string fileName)
+        {
+            char[] newFileName = fileName.ToCharArray();
+            for (int i = 0; i < newFileName.Length; i++)
+            {
+                if (!AndroidAllowedChars.ContainsKey(newFileName[i]))
+                    newFileName[i] = '_';
+            }
+            return new string(newFileName);
+        }
+    }
+}
