8bff926fbaf7bcb9ae5feca887b07fe89fbe7583
Chris Marfia <cmarfia@engagesoftware.com>
2017-07-17 15:02:51 -0500

    Add helper functions to prevent the edit screen from being displayed if the user/content cannot be edited. The action is to redirect the user back to the view if they do not have permission.

--- OpenContent/Edit.ascx.cs
+++ OpenContent/Edit.ascx.cs
@@ -14,6 +14,9 @@ using DotNetNuke.Entities.Modules;
 using DotNetNuke.Common;
 using Satrabel.OpenContent.Components;
 using Satrabel.OpenContent.Components.Alpaca;
+using Satrabel.OpenContent.Components.Manifest;
+using Satrabel.OpenContent.Components.Datasource;
+using DotNetNuke.Common.Utilities;
 
 #endregion
 
@@ -24,6 +27,13 @@ namespace Satrabel.OpenContent
         protected override void OnInit(EventArgs e)
         {
             base.OnInit(e);
+
+            if (!CanEditOrAddContent())
+            {
+                // If the user is not authorized to add/edit this content redirect them back to the default view control.
+                this.Response.Redirect(Globals.NavigateURL(this.TabId));
+            }
+
             var bootstrap = OpenContentControllerFactory.Instance.OpenContentGlobalSettingsController.GetEditLayout() != AlpacaLayoutEnum.DNN;
             bool loadBootstrap = bootstrap && OpenContentControllerFactory.Instance.OpenContentGlobalSettingsController.GetLoadBootstrap();
             hlCancel.NavigateUrl = Globals.NavigateURL();
@@ -38,7 +48,42 @@ namespace Satrabel.OpenContent
             AlpacaContext.Horizontal = OpenContentControllerFactory.Instance.OpenContentGlobalSettingsController.GetEditLayout() == AlpacaLayoutEnum.BootstrapHorizontal;
             AlpacaContext.IsNew = settings.Template.IsListTemplate && string.IsNullOrEmpty(itemId);
         }
+
         public AlpacaContext AlpacaContext { get; private set; }
+
+        /// <summary>Determines whether the current user is authorized to edit this content.</summary>
+        /// <returns><c>true</c> if this current user is authroized to edit this content; otherwise, <c>false</c>.</returns>
+        private bool CanEditOrAddContent()
+        {
+            var id = this.Request.QueryString["id"];
+            var moduleInfo = new OpenContentModuleInfo(this.ModuleConfiguration);
+            var createdByUserid = string.IsNullOrEmpty(id) ? Null.NullInteger : GetCreatedByUserIdForEditContent(moduleInfo, id);
+            return !moduleInfo.Settings.Manifest.DisableEdit
+                && OpenContentUtils.HasEditPermissions(PortalSettings, moduleInfo.ViewModule, moduleInfo.Settings.Manifest.GetEditRole(), createdByUserid);
+        }
+
+        /// <summary>Gets the created by user ID for the content.</summary>
+        /// <param name="moduleInfo">The module information.</param>
+        /// <param name="id">The content ID.</param>
+        /// <returns>The created by user ID or -1 if the content was not found.</returns>
+        private int GetCreatedByUserIdForEditContent(OpenContentModuleInfo moduleInfo, string id)
+        {
+            var ds = DataSourceManager.GetDataSource(moduleInfo.Settings.Manifest.DataSource);
+            var dsContext = OpenContentUtils.CreateDataContext(moduleInfo);
+            IDataItem dsItem = null;
+
+            if (moduleInfo.IsListMode() && !string.IsNullOrEmpty(id))
+            {
+                dsItem = ds.Get(dsContext, id);
+            }
+            else
+            {
+                dsContext.Single = true;
+                dsItem = ds.Get(dsContext, null);
+            }
+
+            return dsItem?.CreatedByUserId ?? Null.NullInteger;
+        }
     }
 }
 
