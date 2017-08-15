9218d2ef57ea56014e01f285c11fb8051715af26
Robrecht Siera <robrecht.siera@holoncom.eu>
2017-08-15 13:14:03 +0200

    simplify

--- OpenContent/Components/AdditionalData/AdditionalDataController.cs
+++ OpenContent/Components/AdditionalData/AdditionalDataController.cs
@@ -64,7 +64,7 @@ namespace Satrabel.OpenContent.Components
             if (versions.Count == 0 || versions[0].Json.ToString() != data.Json)
             {
                 versions.Insert(0, ver);
-                if (versions.Count > App.Services.GlobalSettings(PortalSettings.Current.PortalId).GetMaxVersions())
+                if (versions.Count > App.Services.GlobalSettings().GetMaxVersions())
                 {
                     versions.RemoveAt(versions.Count - 1);
                 }
