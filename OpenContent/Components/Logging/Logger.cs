7d8846c6191755ec82ec5de56e8d4221859b0655
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2017-09-01 10:24:54 +0200

    logger fixe

--- OpenContent/Components/Logging/Logger.cs
+++ OpenContent/Components/Logging/Logger.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+          
 namespace Satrabel.OpenContent.Components.Logging
 {
-    using DotNetNuke.Instrumentation;
-
-    namespace Satrabel.OpenFiles.Components
-    {
         public static class Log
         {
             [Obsolete("This method is obsolete since aug 2017; use App.Services.Logger instead")]
             public static ILogAdapter Logger => App.Services.Logger;
         }
-    }
 }
