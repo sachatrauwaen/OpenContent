dd6c7f59deaf62b71543234e433f6877a99fa418
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2017-09-14 00:01:18 +0200

    increase version to 3.4.1

--- OpenContent/Properties/AssemblyInfo.cs
+++ OpenContent/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("03.04.00.0")]
-[assembly: AssemblyFileVersion("03.04.00.0")]
+[assembly: AssemblyVersion("03.04.01.0")]
+[assembly: AssemblyFileVersion("03.04.01.0")]
