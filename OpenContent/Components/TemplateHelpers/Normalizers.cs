7657baae8be30a8ca25a09d362e9a012225238f0
Robrecht Siera <robrecht.siera@holoncom.eu>
2017-11-23 16:11:04 +0100

    add new normalizer

--- OpenContent/Components/TemplateHelpers/Normalizers.cs
+++ OpenContent/Components/TemplateHelpers/Normalizers.cs
@@ -174,6 +174,15 @@ namespace Satrabel.OpenContent.Components.TemplateHelpers
 
             return Normalize.DynamicValue(value.ToString(), defaultValue);
         }
+
+        public static string JsonValue(JToken value, string defaultValue)
+        {
+            if (value == null) return defaultValue;
+            if (value.IsEmpty()) return defaultValue;
+            if ((value as JValue) == null) return defaultValue;
+
+            return Normalize.DynamicValue(value.ToString(), defaultValue);
+        }
         #endregion
     }
 }
