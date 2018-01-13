69c2b24402e00bd1cb34b5b404ddab1906fa5936
Robrecht Siera <robrecht.siera@holoncom.eu>
2018-01-13 16:18:12 +0100

    add try catch to facilitate better Error message.

--- OpenContent/Components/TemplateHelpers/StringHtmlExtensions.cs
+++ OpenContent/Components/TemplateHelpers/StringHtmlExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -20,74 +21,84 @@ namespace Satrabel.OpenContent.Components.TemplateHelpers
         /// <returns></returns>
         public static string TruncateHtml(this string html, int maxCharacters, string trailingText)
         {
-            if (string.IsNullOrEmpty(html))
-                return html;
-
-            // find the spot to truncate
-            // count the text characters and ignore tags
-            var textCount = 0;
-            var charCount = 0;
-            var ignore = false;
-            foreach (char c in html)
+            try
             {
-                charCount++;
-                if (c == '<')
-                    ignore = true;
-                else if (!ignore)
-                    textCount++;
 
-                if (c == '>')
-                    ignore = false;
 
-                // stop once we hit the limit
-                if (textCount >= maxCharacters)
-                    break;
-            }
+                if (string.IsNullOrEmpty(html))
+                    return html;
 
-            // Truncate the html and keep whole words only
-            var trunc = new StringBuilder(html.TruncateWords(charCount));
-
-            // keep track of open tags and close any tags left open
-            var tags = new Stack<string>();
-            var matches = Regex.Matches(trunc.ToString(),
-                @"<((?<tag>[^\s/>]+)|/(?<closeTag>[^\s>]+)).*?(?<selfClose>/)?\s*>",
-                RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline);
-
-            foreach (Match match in matches)
-            {
-                if (match.Success)
+                // find the spot to truncate
+                // count the text characters and ignore tags
+                var textCount = 0;
+                var charCount = 0;
+                var ignore = false;
+                foreach (char c in html)
                 {
-                    var tag = match.Groups["tag"].Value;
-                    var closeTag = match.Groups["closeTag"].Value;
+                    charCount++;
+                    if (c == '<')
+                        ignore = true;
+                    else if (!ignore)
+                        textCount++;
 
-                    // push to stack if open tag and ignore it if it is self-closing, i.e. <br />
-                    if (!string.IsNullOrEmpty(tag) && string.IsNullOrEmpty(match.Groups["selfClose"].Value))
-                        tags.Push(tag);
+                    if (c == '>')
+                        ignore = false;
 
-                    // pop from stack if close tag
-                    else if (!string.IsNullOrEmpty(closeTag))
+                    // stop once we hit the limit
+                    if (textCount >= maxCharacters)
+                        break;
+                }
+
+                // Truncate the html and keep whole words only
+                var trunc = new StringBuilder(html.TruncateWords(charCount));
+
+                // keep track of open tags and close any tags left open
+                var tags = new Stack<string>();
+                var matches = Regex.Matches(trunc.ToString(),
+                    @"<((?<tag>[^\s/>]+)|/(?<closeTag>[^\s>]+)).*?(?<selfClose>/)?\s*>",
+                    RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.Multiline);
+
+                foreach (Match match in matches)
+                {
+                    if (match.Success)
                     {
-                        // pop the tag to close it.. find the matching opening tag
-                        // ignore any unclosed tags
-                        while (tags.Pop() != closeTag && tags.Count > 0)
-                        { }
+                        var tag = match.Groups["tag"].Value;
+                        var closeTag = match.Groups["closeTag"].Value;
+
+                        // push to stack if open tag and ignore it if it is self-closing, i.e. <br />
+                        if (!string.IsNullOrEmpty(tag) && string.IsNullOrEmpty(match.Groups["selfClose"].Value))
+                            tags.Push(tag);
+
+                        // pop from stack if close tag
+                        else if (!string.IsNullOrEmpty(closeTag))
+                        {
+                            // pop the tag to close it.. find the matching opening tag
+                            // ignore any unclosed tags
+                            while (tags.Pop() != closeTag && tags.Count > 0)
+                            { }
+                        }
                     }
                 }
+
+                if (html.Length > charCount)
+                    // add the trailing text
+                    trunc.Append(trailingText);
+
+                // pop the rest off the stack to close remainder of tags
+                while (tags.Count > 0)
+                {
+                    trunc.Append("</");
+                    trunc.Append(tags.Pop());
+                    trunc.Append('>');
+                }
+
+                return trunc.ToString();
             }
-
-            if (html.Length > charCount)
-                // add the trailing text
-                trunc.Append(trailingText);
-
-            // pop the rest off the stack to close remainder of tags
-            while (tags.Count > 0)
+            catch (Exception ex)
             {
-                trunc.Append("</");
-                trunc.Append(tags.Pop());
-                trunc.Append('>');
+                App.Services.Logger.Error($"Failed to truncated text with max:[{maxCharacters}], error: [{ex.Message}], stacktrace: [{ex.StackTrace}] and text: [{html}]");
+                throw new Exception($"Failed to truncated text. See log for more info.");
             }
-
-            return trunc.ToString();
         }
 
         /// <summary>
@@ -152,8 +163,7 @@ namespace Satrabel.OpenContent.Components.TemplateHelpers
                 return text;
 
             // trunctate the text, then remove the partial word at the end
-            return Regex.Replace(text.Truncate(maxCharacters),
-                       @"\s+[^\s]+$", string.Empty, RegexOptions.IgnoreCase | RegexOptions.Compiled) + trailingText;
+            return Regex.Replace(text.Truncate(maxCharacters), @"\s+[^\s]+$", string.Empty, RegexOptions.IgnoreCase | RegexOptions.Compiled) + trailingText;
         }
 
 
