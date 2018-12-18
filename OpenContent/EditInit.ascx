624fffe694fe6cc8d720e200e656b266206aa858
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2018-12-18 17:10:23 +0100

    Clone module

--- OpenContent/EditInit.ascx
+++ OpenContent/EditInit.ascx
@@ -1,4 +1,44 @@
 <%@ Control Language="C#" AutoEventWireup="false" Inherits="Satrabel.OpenContent.EditInit" CodeBehind="EditInit.ascx.cs" %>
 <%@ Register Src="~/DesktopModules/OpenContent/TemplateInit.ascx" TagPrefix="uc1" TagName="TemplateInit" %>
+<div class="dnnForm" id="tabs-demo">
+    <ul class="dnnAdminTabNav">
+        <li><a href="#ChangeTemplate">Switch Template</a></li>
+        <li><a href="#CloneModule">Clone Module</a></li>
+    </ul>
+    <div id="ChangeTemplate" class="dnnClear">
+        <uc1:TemplateInit runat="server" id="TemplateInitControl" />
+    </div>
+    <div id="CloneModule" class="dnnClear">
+        <ul class="dnnActions dnnClear" style="display: block; padding-left: 35%">
+            
+        </ul>
+        <asp:CheckBoxList ID="cblPages" runat="server" CssClass="TreeView" RepeatLayout="Table" RepeatColumns="5" RepeatDirection="Vertical">
+        </asp:CheckBoxList>
+        <ul class="dnnActions dnnClear" style="display: block; padding-left: 35%">
+            <li>
+                <asp:LinkButton ID="cmdSave" resourcekey="cmdSave" runat="server" CssClass="dnnPrimaryAction" />
+            </li>
+            <li>
+                <asp:HyperLink ID="hlCheckAll" ClientIDMode="Static" resourcekey="cmdCheckAll" runat="server" CssClass="dnnSecondaryAction" NavigateUrl="#" />
+            </li>
+            <li>
+                <asp:HyperLink ID="hlCheckNone" ClientIDMode="Static" resourcekey="cmdCheckNone" runat="server" CssClass="dnnSecondaryAction" NavigateUrl="#" />
+            </li>
+        </ul>
+    </div>
+</div>
 
-<uc1:TemplateInit runat="server" id="TemplateInitControl" />
+<script type="text/javascript">
+    jQuery(function ($) {
+        $('#tabs-demo').dnnTabs();
+
+        $('#hlCheckAll').click(function () {
+            $("#CloneModule input:checkbox:not(:disabled)").prop('checked', true);
+            return false;
+        });
+        $('#hlCheckNone').click(function () {
+            $("#CloneModule input:checkbox:not(:disabled)").prop('checked', false);
+            return false;
+        });
+    });
+</script>
--- OpenContent/EditInit.ascx.cs
+++ OpenContent/EditInit.ascx.cs
@@ -10,7 +10,13 @@
 #region Using Statements
 
 using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using DotNetNuke.Common;
+using DotNetNuke.Common.Utilities;
 using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Tabs;
+using DotNetNuke.Framework.JavaScriptLibraries;
 using Satrabel.OpenContent.Components;
 using Satrabel.OpenContent.Components.Manifest;
 
@@ -20,6 +26,8 @@ namespace Satrabel.OpenContent
 {
     public partial class EditInit : PortalModuleBase
     {
+        
+
         protected override void OnInit(EventArgs e)
         {
             base.OnInit(e);
@@ -32,6 +40,101 @@ namespace Satrabel.OpenContent
             ti.Settings = settings;
             ti.Renderinfo = renderinfo;
             ti.RenderOnlySaveButton = true;
+
+            
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            
+            JavaScript.RequestRegistration(CommonJs.DnnPlugins);
+            cmdSave.Click += cmdSave_Click;
+
+            var m = ModuleContext.Configuration;
+            TabController tc = new TabController();
+            var mc = new ModuleController();
+            // its the same for each module -> to cache
+            var til = TabController.GetTabsBySortOrder(PortalSettings.PortalId, m.CultureCode, true)
+                .Where(t => /*!t.IsDeleted &&*/ t.ParentId != PortalSettings.AdminTabId 
+                        && t.TabID != PortalSettings.AdminTabId 
+                        && t.CultureCode == m.CultureCode);
+            //var tid = tc.GetTabsByModuleID(m.ModuleID);
+            var tmiLst = mc.GetAllTabsModulesByModuleID(m.ModuleID).Cast<ModuleInfo>();
+
+            foreach (TabInfo ti in til)
+            {
+                {
+                    ListItem li = new ListItem(ti.TabName, ti.TabID.ToString());
+                    li.Text = ti.IndentedTabName;
+                    li.Enabled = ti.TabID != m.TabID;
+                    cblPages.Items.Add(li);
+
+                    ModuleInfo tmi = tmiLst.SingleOrDefault(t => t.TabID == ti.TabID);
+
+                    //if (tid.Keys.Contains(ti.TabID))
+                    if (tmi != null)
+                    {
+                        if (tmi.IsDeleted)
+                        {
+                            //li.Enabled = false;
+                            li.Text = "<i>" + li.Text + "</i>";
+                        }
+                        else
+                        {
+                            li.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void cmdSave_Click(object sender, EventArgs e)
+        {
+            var mi = ModuleContext.Configuration;
+            var mc = new ModuleController();
+            TabController tc = new TabController();
+            var tabModules = mc.GetAllTabsModulesByModuleID(mi.ModuleID).Cast<ModuleInfo>().Where(t => t.IsDeleted == false);
+            foreach (ListItem li in cblPages.Items)
+            {
+                if (li.Enabled)
+                {
+                    bool Add = li.Selected;
+                    int TabId = int.Parse(li.Value);
+                    if (Add && !tabModules.Any(m => m.TabID == TabId))
+                    {
+                        ModuleInfo sourceModule = mc.GetModule(mi.ModuleID, mi.TabID, false);
+                        TabInfo destinationTab = tc.GetTab(TabId, PortalSettings.PortalId, false);
+
+                        ModuleInfo tmpModule = mc.GetModule(mi.ModuleID, TabId, false);
+                        if (tmpModule != null && tmpModule.IsDeleted)
+                        {
+                            mc.RestoreModule(tmpModule);
+                        }
+                        else
+                        {
+                            mc.CopyModule(sourceModule, destinationTab, Null.NullString, true);
+                        }
+                        if (sourceModule.DefaultLanguageModule != null && destinationTab.DefaultLanguageTab != null) // not default language
+                        {
+                            ModuleInfo defaultLanguageModule = mc.GetModule(sourceModule.DefaultLanguageModule.ModuleID, destinationTab.DefaultLanguageTab.TabID, false);
+                            if (defaultLanguageModule != null)
+                            {
+                                ModuleInfo destinationModule = destinationModule = mc.GetModule(sourceModule.ModuleID, destinationTab.TabID, false);
+                                destinationModule.DefaultLanguageGuid = defaultLanguageModule.UniqueId;
+                                mc.UpdateModule(destinationModule);
+                            }
+                        }
+
+                    }
+                    else if (!Add && tabModules.Any(m => m.TabID == TabId))
+                    {
+                        mc.DeleteTabModule(TabId, mi.ModuleID, true);
+                    }
+                }
+            }
+            DataCache.ClearCache();
+            Response.Redirect(Globals.NavigateURL(), true);
         }
 
         protected override void OnPreRender(EventArgs e)
@@ -39,6 +142,10 @@ namespace Satrabel.OpenContent
             OpenContent.TemplateInit ti = (TemplateInit)TemplateInitControl;
             ti.RenderInitForm();
         }
+
+
+
+
     }
 
 }
