caf2a025ce80352b9162abba4f8789f683359d73
Robrecht Siera <robrecht.siera@holoncom.eu>
2019-05-29 17:16:08 +0200

    sync dll version with .dnn version

--- OpenContent/Properties/AssemblyInfo.cs
+++ OpenContent/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Satrabel")]
 [assembly: AssemblyProduct("OpenContent")]
-[assembly: AssemblyCopyright("Copyright © 2015-2017")]
+[assembly: AssemblyCopyright("Copyright © 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("04.00.00.0")]
-[assembly: AssemblyFileVersion("04.00.00.0")]
+[assembly: AssemblyVersion("04.02.00.0")]
+[assembly: AssemblyFileVersion("04.02.00.0")]
