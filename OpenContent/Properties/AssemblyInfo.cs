a921e87792861f6cfebe6d9afdf6bdd2acbaad53
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2019-07-24 10:15:24 +0200

    v4.3

--- OpenContent/Properties/AssemblyInfo.cs
+++ OpenContent/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("04.02.00.0")]
-[assembly: AssemblyFileVersion("04.02.00.0")]
+[assembly: AssemblyVersion("04.03.00.0")]
+[assembly: AssemblyFileVersion("04.03.00.0")]
