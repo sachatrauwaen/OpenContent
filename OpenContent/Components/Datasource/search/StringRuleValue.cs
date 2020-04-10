0c768cb4f11f8292d60b5227c6f05fb6272457ef
Sacha Trauwaen <sachatrauwaen@yahoo.com>
2020-04-10 18:48:22 +0200

    fix filters on number fields

--- OpenContent/Components/Datasource/search/StringRuleValue.cs
+++ OpenContent/Components/Datasource/search/StringRuleValue.cs
@@ -8,5 +8,10 @@
             _value = value;
         }
         public override string AsString => _value;
+
+        public override float AsFloat => float.Parse(_value);
+        public override int AsInteger => int.Parse(_value);
+        public override bool AsBoolean => bool.Parse(_value);
+        public override long AsLong => long.Parse(_value);
     }
 }
