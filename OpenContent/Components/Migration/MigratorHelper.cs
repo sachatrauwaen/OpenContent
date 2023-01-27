510977745ea0e26047aa6fe21fea569dbf99c2d9
holoncom <github.rsiera@holoncom.eu>
2023-01-27 06:11:22 +0100

    Migrator - Add File2ToImageX migrator

--- OpenContent/Components/Migration/MigratorHelper.cs
+++ OpenContent/Components/Migration/MigratorHelper.cs
@@ -12,18 +12,91 @@ namespace Satrabel.OpenContent.Components.Migration
         /// <exception cref="NotImplementedException"></exception>
         public static JToken ConvertTo(JToken sourceData, OcFieldInfo sourceField, OcFieldInfo targetField, int portalId, int moduleID)
         {
-            if (sourceField.Type == "image2" && targetField.Type == "imagex")
+            var migratorType = $"{sourceField.Type} => {targetField.Type}";
+
+            switch (migratorType)
             {
-                return MigratorHelper.Image2ToImageX(sourceData, sourceField, targetField, portalId, moduleID);
+                case "file2 => imagex":
+                    return MigratorHelper.File2ToImageX(sourceData, sourceField, targetField, portalId, moduleID);
+                case "image2 => imagex":
+                    return MigratorHelper.Image2ToImageX(sourceData, sourceField, targetField, portalId, moduleID);
+                case "text => textarea":
+                    return MigratorHelper.TextToTextArea(sourceData, sourceField, targetField);
+                default:
+                    throw new NotImplementedException($"Migration from field type {sourceField.Type} to {targetField.Type} is not supported. Consider implementing it yourself.");
             }
-            if (sourceField.Type == "text" && targetField.Type == "textarea")
-            {
-                return MigratorHelper.TextToTextArea(sourceData, sourceField, targetField);
-            }
-            throw new NotImplementedException($"Migration from field type {sourceField.Type} to {targetField.Type} is not supported. Consider implementing it yourself.");
         }
 
-        public static JToken Image2ToImageX(JToken input, OcFieldInfo sourceField, OcFieldInfo targetField, int portalId, int moduleId)
+        private static JToken File2ToImageX(JToken input, OcFieldInfo sourceField, OcFieldInfo targetField, int portalId, int moduleId)
+        {
+            var fileManager = FileManager.Instance;
+            var folderManager = FolderManager.Instance;
+            int fileId = int.Parse(input.ToString());
+            var file = fileManager.GetFile(fileId);
+
+            // check if extention is allowed in target field
+            if (!targetField.Options["fileExtensions"].Value<string>().ToLowerInvariant().Contains(file.Extension.ToLowerInvariant())) return new JObject(); 
+
+            if (sourceField.Options["folder"] != targetField.Options["uploadfolder"])
+            {
+                bool secure = targetField.Options["secure"] != null && targetField.Options["secure"].Value<Boolean>();
+                string uploadParentFolder = "OpenContent/" + (secure ? "Secure" : "") + "Files/";
+                var parentFolder = folderManager.GetFolder(portalId, uploadParentFolder);
+                if (parentFolder == null)
+                {
+                    if (secure)
+                    {
+                        var folderMapping = FolderMappingController.Instance.GetFolderMapping(portalId, "Secure");
+                        folderManager.AddFolder(folderMapping, uploadParentFolder);
+                    }
+                    else
+                    {
+                        folderManager.AddFolder(portalId, uploadParentFolder);
+                    }
+                }
+                string uploadfolder = "OpenContent/" + (secure ? "Secure" : "") + "Files/" + moduleId + "/";
+                //if (module.Settings.Manifest.DeleteFiles)
+                //{
+                //    if (!string.IsNullOrEmpty(context.Request.Form["itemKey"]))
+                //    {
+                //        uploadfolder += context.Request.Form["itemKey"];
+                //    }
+                //}
+                if (targetField.Options["uploadfolder"] != null &&
+                    !string.IsNullOrEmpty(targetField.Options["uploadfolder"].ToString()))
+                {
+                    uploadfolder = targetField.Options["uploadfolder"].ToString();
+                }
+                var targetFolder = folderManager.GetFolder(portalId, uploadfolder);
+                if (targetFolder == null)
+                {
+                    if (secure)
+                    {
+                        var folderMapping = FolderMappingController.Instance.GetFolderMapping(portalId, "Secure");
+                        targetFolder = folderManager.AddFolder(folderMapping, uploadfolder);
+                    }
+                    else
+                    {
+                        targetFolder = folderManager.AddFolder(portalId, uploadfolder);
+                    }
+                }
+                file = fileManager.CopyFile(file, targetFolder);
+            }
+
+            // create ImageX JToken, but also copy the original image to the new image location
+            JToken output = new JObject();
+            output["id"] = file.FileId.ToString();
+            output["url"] = fileManager.GetUrl(file);
+            output["filename"] = file.FileName;
+            output["width"] = file.Width;
+            output["height"] = file.Height;
+            //output["cropUrl"] = "";
+            //output["crop"] = new JObject();
+
+            return output;
+        }
+
+        private static JToken Image2ToImageX(JToken input, OcFieldInfo sourceField, OcFieldInfo targetField, int portalId, int moduleId)
         {
             var fileManager = FileManager.Instance;
             var folderManager = FolderManager.Instance;
@@ -89,7 +162,7 @@ namespace Satrabel.OpenContent.Components.Migration
             return output;
         }
 
-        public static JToken TextToTextArea(JToken input, OcFieldInfo sourceField, OcFieldInfo targetField)
+        private static JToken TextToTextArea(JToken input, OcFieldInfo sourceField, OcFieldInfo targetField)
         {
             // create TextArea JToken from the original data
             JToken output = new JValue(input.ToString());
