7e9a7ca5c27b6d316dbf6156299e30cb7c39cb20
holoncom <github.rsiera@holoncom.eu>
2023-01-30 15:49:11 +0100

    Migrator - improve handling of already running jobs

--- OpenContent/Components/Migration/Migrator.cs
+++ OpenContent/Components/Migration/Migrator.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using System.Web;
 using System.Web.Hosting;
 using DotNetNuke.Entities.Modules;
